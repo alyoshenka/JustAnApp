e06c13787b7b96544db8ab0404ad6582ca3fd889
Alexi <alexibmost@gmail.com>
2022-04-25 10:12:02 -0700

    changed buttons, can navigate to activities

--- app/src/main/java/com/alyoshenka/justanapp/MainActivity.kt
+++ app/src/main/java/com/alyoshenka/justanapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.alyoshenka.justanapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -15,9 +17,19 @@ class MainActivity : AppCompatActivity() {
         val bl = findViewById<Button>(R.id.button_bl)
         val br = findViewById<Button>(R.id.button_br)
 
-        tl.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_tl), Toast.LENGTH_SHORT).show() }
+        // tl.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_tl), Toast.LENGTH_SHORT).show() }
         tr.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_tr), Toast.LENGTH_SHORT).show() }
-        bl.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_bl), Toast.LENGTH_SHORT).show() }
+        // bl.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_bl), Toast.LENGTH_SHORT).show() }
         br.setOnClickListener { Toast.makeText(this, resources.getString(R.string.button_br), Toast.LENGTH_SHORT).show() }
+    }
+
+    fun viewMovies(view: View) {
+        intent = Intent(this, ViewMoviesActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun viewMovieDetails(view: View) {
+        intent = Intent(this, ViewMovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
