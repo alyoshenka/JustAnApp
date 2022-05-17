0bb190748e33434f29691e43bb529e8dc36820ca
Alexi <alexibmost@gmail.com>
2022-05-17 12:52:32 -0700

    a5 - firebase connection issue

---
+++ app/src/main/java/com/alyoshenka/justanapp/FirebaseActivity.kt
@@ -0,0 +1,11 @@
+package com.alyoshenka.justanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirebaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase)
+    }
+}
--- app/src/main/java/com/alyoshenka/justanapp/MainActivity.kt
+++ app/src/main/java/com/alyoshenka/justanapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.alyoshenka.justanapp
 
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +8,21 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import android.R.attr.password
+import android.content.Intent
+import android.content.SharedPreferences
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.auth.AuthResult
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,5 +48,75 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun signIn(view: View) {
+        Log.d("FIREBASE", "signIn")
+
+        // 1 - validate display name, email, and password entries
+        val displayname = findViewById<TextView>(R.id.username).text.toString()
+        val email = findViewById<TextView>(R.id.email).text.toString()
+        val password = findViewById<TextView>(R.id.password).text.toString()
+        Log.d("FIREBASE", "user: " + email)
+        Log.d("FIREBASE", "password: " + password)
+
+        // should always save username
+        savePreference(DISPLAYNAME, displayname)
+
+        // 2 - save valid entries to shared preferences
+
+
+        // 3 - sign into Firebase
+        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this@MainActivity, OnCompleteListener<AuthResult> { task ->
+                Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful)
+                if (task.isSuccessful) {
+                    // should only save credentials on success
+                    savePreference(PASSWORD, password)
+                    savePreference(EMAIL, email)
+                    // update profile. displayname is the value entered in UI
+                    val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+                    val profileUpdates: UserProfileChangeRequest = UserProfileChangeRequest.Builder()
+                        .setDisplayName(displayname)
+                        .build()
+                    user?.updateProfile(profileUpdates)
+                        ?.addOnCompleteListener(OnCompleteListener<Void?> { task ->
+                            if (task.isSuccessful) {
+                                Log.d("FIREBASE", "User profile updated.")
+                                // Go to FirebaseActivity
+                                startActivity(
+                                    Intent(
+                                        this@MainActivity,
+                                        FirebaseActivity::class.java
+                                    )
+                                )
+                            }
+                        })
+                } else {
+                    Log.d("FIREBASE", "sign-in failed")
+                    Toast.makeText(
+                        this@MainActivity, "Sign In Failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    private fun savePreference(title : String, data : String) {
+        val prefs = this@MainActivity.getSharedPreferences("com.alyoshenka.justanapp", 0)
+        val editor = prefs.edit()
+        editor.putString(title, data)
+        editor.commit()
+    }
+
+    private fun getPreference(name : String) : String? {
+        return this@MainActivity.getSharedPreferences("com.alyoshenka.justanapp", 0).getString(name, "")
+    }
+
+    companion object {
+        val DISPLAYNAME = "displayname"
+        val EMAIL = "email"
+        val PASSWORD = "password"
     }
 }
